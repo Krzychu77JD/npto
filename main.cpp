138002346200d6603013bd146b73b0b37ecf7cb3
Krzysztof Wojszkowicz <k.wojszkowicz@gmail.com>
2024-04-29 12:54:28 +0200

    pierwszy commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b, wynik;
+    cout << "Wpisz a: " << endl;
+    cin>> a;
+    cout << "Wpisz b: " << endl;
+    cin >> b;
+    wynik=a*b;
+    cout << "Wynik to:" << wynik <<endl;
+
+    return 0;
+
+}

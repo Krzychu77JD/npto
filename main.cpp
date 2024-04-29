9d60a6e3ff96e7875e64286f2e3a41cc4b21476f
Igor Ignatiuk <ignatiukigor48@gmail.com>
2024-04-29 13:21:04 +0200

    Poprawka

--- main.cpp
+++ main.cpp
@@ -9,8 +9,7 @@ int main()
     cin>> a;
     cout << "Wpisz b: " << endl;
     cin >> b;
-    wynik=a*b;
-    cout << "Wynik to:" << wynik <<endl;
+    cout << "Wynik to:" << a*b <<endl;
 
     return 0;
 
